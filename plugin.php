9b10d4203e2895cb4fde874a0443eccaa7fe96f0
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-08-11 15:20:59 +0200

    fix piwik url

--- plugin.php
+++ plugin.php
@@ -13,7 +13,7 @@ Author URI: https://solsort.com
 
 
 function script_solsortjs() {
-  echo "<script type=\"text/javascript\"> var _paq = _paq || []; _paq.push([\"setCookieDomain\", \"*.solsort.com\"]); _paq.push([\"setDomains\", [\"*.solsort.com\"]]); _paq.push(['trackPageView']); _paq.push(['enableLinkTracking']); (function() { var u=\"/piwik/\"; _paq.push(['setTrackerUrl', u+'piwik.php']); _paq.push(['setSiteId', 1]); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s); })(); </script><noscript><p><img src=\"/piwik/piwik.php?idsite=1\" style=\"border:0;\" alt=\"\" /></p></noscript>";
+  echo "<script type=\"text/javascript\"> var _paq = _paq || []; _paq.push([\"setCookieDomain\", \"*.solsort.com\"]); _paq.push([\"setDomains\", [\"*.solsort.com\"]]); _paq.push(['trackPageView']); _paq.push(['enableLinkTracking']); (function() { var u=\"//blog.solsort.com/piwik/\"; _paq.push(['setTrackerUrl', u+'piwik.php']); _paq.push(['setSiteId', 1]); var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s); })(); </script><noscript><p><img src=\"/blog.solsort.com/piwik/piwik.php?idsite=1\" style=\"border:0;\" alt=\"\" /></p></noscript>";
 	echo '<script src="/solsort.js"></script>';
 }
 
